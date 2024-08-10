1bd762ad78058b97dca72e654985c1b00f62e67e
DharamThisSide <dharam200123pal@gmail.com>
2024-08-10 11:07:19 +0000

    Conversion of Data types

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -26,17 +26,75 @@ console.log(valueInNumber2) //NaN
 // "33abc"=>NaN
 //true => 1; false=>0
 
-let isLoggedIn=1
+let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn); // true
 
 // 1=> true ; 0=> false
 // ""=>false; "dharam"=>true
 
-let someNumber=33
+let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+console.log(stringNumber);//33
+console.log(typeof stringNumber); //string
+
+// ****************Operations********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/4);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " dharam"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122 //string pehle to puri string
+console.log(1 + 2 + "2"); // 32  //number pehle to add krdo fir string
+
+
+console.log(true);
+console.log(+true); // 1
+
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // bad way
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1;
+console.log(gameCounter1);
+
+let gameCounter2= 100
+// gameCounter2++;
+console.log(gameCounter2++);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
