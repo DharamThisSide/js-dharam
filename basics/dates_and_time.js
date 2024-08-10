6b705b7515278a5c7cfada55993ec7f5c64de298
DharamThisSide <dharam200123pal@gmail.com>
2024-08-10 17:21:48 +0000

    date and time

---
+++ basics/dates_and_time.js
@@ -0,0 +1,42 @@
+// dates
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+console.log(typeof mydate);  // object
+
+let myCreatedDate= new Date(2023,0,23) // year, month(0 se start), day
+let myCreatedDate1= new Date(2023,0,23,5,3)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate1);
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let date= new Date("2023-01-14")
+// console.log(date.toLocaleString());
+
+let myTimeStamp= Date.now()
+// console.log(myTimeStamp);
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+// console.log(newDate);
+
+// console.log(newDate.getMonth()+1)
+// console.log(newDate.getDay())
+
+`${newDate.getDay()} and the time`
+
+
+
+
+
+
+
+
+
+
+ 
