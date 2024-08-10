eb365e0882bcf6124363f8f073642b5f0b28bf9d
DharamThisSide <dharam200123pal@gmail.com>
2024-08-10 02:24:49 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use struct"; // treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+let name="dharampal"
+let age=18
+let isLoggedIn= true
+
+// number, bigint, string, boolean
+// null => standalone value
+//undefined => 
+//symbol => unique
+
+// object
+
+console.log(typeof(name))
+console.log(typeof(null))  // object
+console.log(typeof(undefined))//undefined
+
