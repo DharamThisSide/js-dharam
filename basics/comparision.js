4dff270fb609bec06a3d4ea1064abb291b97238f
DharamThisSide <dharam200123pal@gmail.com>
2024-08-10 11:43:29 +0000

    comparison operators

---
+++ basics/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(2<=1);
+// console.log(2<2);
+// console.log(2!=1);
+
+
+console.log("2">1);//true apne app number me convert krke check karega
+console.log("02">1);
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+console.log(undefined==0);// in all acses false
+
+// == and other comparision work differently in javascript
+
+//=== strict check
+console.log("2"===2); // false
+
+
+
+
+
